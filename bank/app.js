68e7e1d2a52f9e56c29955b1c60c129b48f55cbc
Oakley Dye <oakley.dye@gmail.com>
2021-01-11 07:44:13 -0700

    adding bank app files

---
+++ bank/app.js
@@ -0,0 +1,59 @@
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+};
+
+function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+
+    if (!route) {
+        return navigate('/login');
+    }
+  
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+}
+
+function navigate(path) {
+    window.history.pushState({}, path, window.location.origin + path);
+    updateRoute();
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+}
+
+async function register() {
+    const registerForm = document.getElementById('registerForm');
+    const formData = new FormData(registerForm);
+    const data = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(data);
+    const result = await createAccount(jsonData);
+
+    if (result.error) {
+        return console.log('An error occured:', result.error);
+    }
+    
+    console.log('Account created!', result);
+}
+
+async function createAccount(account) {
+    try {
+      const response = await fetch('//localhost:5000/api/accounts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: account
+      });
+      return await response.json();
+    } catch (error) {
+      return { error: error.message || 'Unknown error' };
+    }
+}
+
+window.onpopstate = () => updateRoute();
+updateRoute();
